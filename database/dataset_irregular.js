352ad58030dd44a65f63b190b82825364261666f
Ganis Z S <ganiszul.fa@gmail.com>
2015-06-17 16:34:09 +0900

    Create dataset_irregular.js

---
+++ database/dataset_irregular.js
@@ -0,0 +1,121 @@
+
+if (1) {
+		idx++;
+		explain_set[idx] = "불규칙";
+		
+		//01
+		word_set1_arr[idx].push('it is pretty');
+		word_set2_arr[idx].push('예뻐요');
+		
+		word_set1_arr[idx].push('i am busy');
+		word_set2_arr[idx].push('바빠요');
+		
+		word_set1_arr[idx].push('i write');
+		word_set2_arr[idx].push('써요');
+		
+		word_set1_arr[idx].push('i walk');
+		word_set2_arr[idx].push('걸어요');
+		
+		word_set1_arr[idx].push('i walked');
+		word_set2_arr[idx].push('걸었어요');
+		
+		//06
+		word_set1_arr[idx].push('i will walk');
+		word_set2_arr[idx].push('걸을 거예요');
+		
+		word_set1_arr[idx].push('i listen');
+		word_set2_arr[idx].push('들어요');
+		
+		word_set1_arr[idx].push('i want to listen');
+		word_set2_arr[idx].push('듣고 싶어요');
+		
+		word_set1_arr[idx].push('i ask');
+		word_set2_arr[idx].push('묻어요');
+		
+		word_set1_arr[idx].push('i want to ask');
+		word_set2_arr[idx].push('묻고 싶어요');
+		
+		//11
+		word_set1_arr[idx].push('i close it');
+		word_set2_arr[idx].push('닫아요');
+		
+		word_set1_arr[idx].push('i receive it');
+		word_set2_arr[idx].push('받아요');
+		
+		word_set1_arr[idx].push('i believe it');
+		word_set2_arr[idx].push('믿어요');
+		
+		word_set1_arr[idx].push('it is easy');
+		word_set2_arr[idx].push('쉬워요');
+		
+		word_set1_arr[idx].push('it is cold');
+		word_set2_arr[idx].push('추워요');
+		
+		//16
+		word_set1_arr[idx].push('it is difficult');
+		word_set2_arr[idx].push('어려워요');
+		
+		word_set1_arr[idx].push('it is a hot weather');
+		word_set2_arr[idx].push('더워요');
+		
+		word_set1_arr[idx].push('it is heavy');
+		word_set2_arr[idx].push('무거워요');
+		
+		word_set1_arr[idx].push('it is light');
+		word_set2_arr[idx].push('가벼워요');
+		
+		word_set1_arr[idx].push('help me');
+		word_set2_arr[idx].push('도와요');
+		//20
+		
+		word_set1_arr[idx].push('i wear');
+		word_set2_arr[idx].push('입어요');
+		
+		word_set1_arr[idx].push('it is narrow');
+		word_set2_arr[idx].push('좁아요');
+		
+		word_set1_arr[idx].push('i do not know');
+		word_set2_arr[idx].push('몰라요');
+		
+		word_set1_arr[idx].push('i call');
+		word_set2_arr[idx].push('뷸라요');
+		
+		word_set1_arr[idx].push('it is fast');
+		word_set2_arr[idx].push('빨라요');
+		//25
+		
+		word_set1_arr[idx].push('it is different');
+		word_set2_arr[idx].push('달라요');
+		
+		word_set1_arr[idx].push('please make it (polite)');
+		word_set2_arr[idx].push('만드 세요');
+		
+		word_set1_arr[idx].push('i know (polite)');
+		word_set2_arr[idx].push('압니다');
+		
+		word_set1_arr[idx].push('it is a long skirt');
+		word_set2_arr[idx].push('긴 치마');
+		
+		word_set1_arr[idx].push('i make (polite)');
+		word_set2_arr[idx].push('만듭니다');
+		//30
+		
+		word_set1_arr[idx].push('please play it (polite)');
+		word_set2_arr[idx].push('노세요');
+		
+		word_set1_arr[idx].push('it is far (polite)');
+		word_set2_arr[idx].push('멉니다');
+		
+		word_set1_arr[idx].push('it is long (polite)');
+		word_set2_arr[idx].push('깁니다');
+		
+		word_set1_arr[idx].push('to live + 는');
+		word_set2_arr[idx].push('사는');
+		
+		word_set1_arr[idx].push('to make + 는');
+		word_set2_arr[idx].push('만드는');
+		//END HERE
+		
+	}
+	
+	
