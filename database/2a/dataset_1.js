7f5ba5773c1d740981beaa3ff4ec106d30cf6f4f
Ganis Z S <ganiszul.fa@gmail.com>
2015-06-17 16:31:40 +0900

    Create dataset_1.js

---
+++ database/2a/dataset_1.js
@@ -0,0 +1,630 @@
+
+if (true) {
+		idx++;
+		explain_set[idx] = "단어 1과 - a";
+		
+		//01
+		word_set1_arr[idx].push('to attend');
+		word_set2_arr[idx].push('(에) 다니다');
+		
+		word_set1_arr[idx].push('to inform');
+		word_set2_arr[idx].push('알리다');
+		
+		word_set1_arr[idx].push('contact information');
+		word_set2_arr[idx].push('연락처');
+		
+		word_set1_arr[idx].push('e-mail address');
+		word_set2_arr[idx].push('이메일 주소');
+		
+		word_set1_arr[idx].push('What is your name?');
+		word_set2_arr[idx].push('이름이 어떻게 되세요?');
+		
+		//06
+		word_set1_arr[idx].push('Where are you from?');
+		word_set2_arr[idx].push('어느 나라에서 오셨어요');
+		
+		word_set1_arr[idx].push('Just a moment');
+		word_set2_arr[idx].push('잠깐만요');
+		
+		word_set1_arr[idx].push('How do you do?');
+		word_set2_arr[idx].push('처음 뵙겠습니다');
+		
+		word_set1_arr[idx].push('What do you do for a living?');
+		word_set2_arr[idx].push('무슨 일 하세요');
+		
+		word_set1_arr[idx].push('to play basketball');
+		word_set2_arr[idx].push('농구하다');
+		
+		//11
+		word_set1_arr[idx].push('to get to know과 - person)');
+		word_set2_arr[idx].push('사귀다');
+		
+		word_set1_arr[idx].push('to major in, to specialize in');
+		word_set2_arr[idx].push('전공하다');
+		
+		word_set1_arr[idx].push('to be beautiful');
+		word_set2_arr[idx].push('아름답다');
+		
+		word_set1_arr[idx].push('to be kind');
+		word_set2_arr[idx].push('친절하다');
+		
+		word_set1_arr[idx].push('to be convinient, to be comfortable');
+		word_set2_arr[idx].push('편하다');
+		
+		//16
+		word_set1_arr[idx].push('hometown');
+		word_set2_arr[idx].push('고향');
+		
+		word_set1_arr[idx].push('city');
+		word_set2_arr[idx].push('도시');
+		
+		word_set1_arr[idx].push('all, every');
+		word_set2_arr[idx].push('모두');
+		
+		word_set1_arr[idx].push('culture');
+		word_set2_arr[idx].push('문화');
+		
+		word_set1_arr[idx].push('now');
+		word_set2_arr[idx].push('이제');
+		//END HERE
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "단어 1과 - b";
+		
+		//01
+		word_set1_arr[idx].push('hobby');
+		word_set2_arr[idx].push('취미');
+		
+		word_set1_arr[idx].push('again');
+		word_set2_arr[idx].push('다시');
+		
+		word_set1_arr[idx].push('the most');
+		word_set2_arr[idx].push('제일');
+		
+		word_set1_arr[idx].push('also');
+		word_set2_arr[idx].push('또');
+		
+		word_set1_arr[idx].push('but');
+		word_set2_arr[idx].push('하지만');
+		
+		//06
+		word_set1_arr[idx].push('I can\'t do과 - it) well yet');
+		word_set2_arr[idx].push('아직 잘 못해요');
+		
+		word_set1_arr[idx].push('It\s really good');
+		word_set2_arr[idx].push('참 좋아요');
+		
+		word_set1_arr[idx].push('to look around');
+		word_set2_arr[idx].push('구경하다');
+		
+		word_set1_arr[idx].push('five months');
+		word_set2_arr[idx].push('5개월');
+		
+		word_set1_arr[idx].push('one month');
+		word_set2_arr[idx].push('한 달');
+		
+		//11
+		word_set1_arr[idx].push('one week');
+		word_set2_arr[idx].push('1주일');
+		
+		word_set1_arr[idx].push('relative');
+		word_set2_arr[idx].push('친척');
+		
+		word_set1_arr[idx].push('here and there');
+		word_set2_arr[idx].push('여기저기');
+		
+		word_set1_arr[idx].push('come in please');
+		word_set2_arr[idx].push('들어오세요');
+		
+		word_set1_arr[idx].push('Good Job!');
+		word_set2_arr[idx].push('수고하셨습니다');
+		
+		//16
+		word_set1_arr[idx].push('Please sit here');
+		word_set2_arr[idx].push('여기 앉으세요');
+		
+		word_set1_arr[idx].push('I\'m sorry');
+		word_set2_arr[idx].push('죄송합니다');
+		
+		//END HERE
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "단어 2과 - a";
+		
+		//01
+		word_set1_arr[idx].push('to review');
+		word_set2_arr[idx].push('복습하다');
+		
+		word_set1_arr[idx].push('to preview');
+		word_set2_arr[idx].push('예습하다');
+		
+		word_set1_arr[idx].push('to jog');
+		word_set2_arr[idx].push('조깅하다');
+		
+		word_set1_arr[idx].push('to prepare');
+		word_set2_arr[idx].push('준비하다');
+		
+		word_set1_arr[idx].push('to talk on the phone');
+		word_set2_arr[idx].push('통화하다');
+		
+		//06
+		word_set1_arr[idx].push('to have a meeting');
+		word_set2_arr[idx].push('회의하다');
+		
+		word_set1_arr[idx].push('graduate school');
+		word_set2_arr[idx].push('대학원');
+		
+		word_set1_arr[idx].push('interpretation');
+		word_set2_arr[idx].push('통역');
+		
+		word_set1_arr[idx].push('later');
+		word_set2_arr[idx].push('이따가');
+		
+		word_set1_arr[idx].push('to memorize words');
+		word_set2_arr[idx].push('단어를 외우다');
+		
+		//11
+		word_set1_arr[idx].push('to run a restaurant');
+		word_set2_arr[idx].push('식당을 하다');
+		
+		word_set1_arr[idx].push('to check mail');
+		word_set2_arr[idx].push('이메일을 확인하다');
+		
+		word_set1_arr[idx].push('to finish all work');
+		word_set2_arr[idx].push('일을 다 하다');
+		
+		word_set1_arr[idx].push('to apply for a job');
+		word_set2_arr[idx].push('회사에 들어가다');
+		
+		word_set1_arr[idx].push('I see');
+		word_set2_arr[idx].push('알겠어요');
+		
+		//16
+		word_set1_arr[idx].push('What brings you here?');
+		word_set2_arr[idx].push('웬일이세요?');
+		
+		word_set1_arr[idx].push('to quit a job');
+		word_set2_arr[idx].push('그만두다');
+		
+		word_set1_arr[idx].push('to be surprised');
+		word_set2_arr[idx].push('놀라다');
+		
+		word_set1_arr[idx].push('to look into');
+		word_set2_arr[idx].push('알아보다');
+		
+		word_set1_arr[idx].push('to be sleepy');
+		word_set2_arr[idx].push('졸리다');
+		//END HERE
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "단어 2과 - b";
+		
+		//01
+		word_set1_arr[idx].push('to be late');
+		word_set2_arr[idx].push('지각하다');
+		
+		word_set1_arr[idx].push('to be angry');
+		word_set2_arr[idx].push('화가 나다');
+		
+		word_set1_arr[idx].push('office');
+		word_set2_arr[idx].push('사무실');
+		
+		word_set1_arr[idx].push('the president of a company');
+		word_set2_arr[idx].push('사장님');
+		
+		word_set1_arr[idx].push('29 years old');
+		word_set2_arr[idx].push('스물 아홉 살');
+		
+		//06
+		word_set1_arr[idx].push('alarm sound');
+		word_set2_arr[idx].push('알람 소리');
+		
+		word_set1_arr[idx].push('finally');
+		word_set2_arr[idx].push('드디어');
+		
+		word_set1_arr[idx].push('to be startled');
+		word_set2_arr[idx].push('깜짝 놀라다');
+		
+		word_set1_arr[idx].push('to open eyes');
+		word_set2_arr[idx].push('눈을 뜨다');
+		
+		word_set1_arr[idx].push('to get off the bus');
+		word_set2_arr[idx].push('버스에서 내리다');
+		
+		//11
+		word_set1_arr[idx].push('to set an alarm clock');
+		word_set2_arr[idx].push('알라시계를 맞추다');
+		
+		word_set1_arr[idx].push('to register');
+		word_set2_arr[idx].push('등록하다');
+		
+		word_set1_arr[idx].push('speaking');
+		word_set2_arr[idx].push('말하다');
+		
+		word_set1_arr[idx].push('every day');
+		word_set2_arr[idx].push('매일');
+		
+		word_set1_arr[idx].push('child');
+		word_set2_arr[idx].push('어른');
+		
+		//16
+		word_set1_arr[idx].push('adult');
+		word_set2_arr[idx].push('어른');
+		
+		word_set1_arr[idx].push('this year');
+		word_set2_arr[idx].push('올해');
+		
+		word_set1_arr[idx].push('weekdays');
+		word_set2_arr[idx].push('주중');
+		
+		word_set1_arr[idx].push('academy');
+		word_set2_arr[idx].push('학원');
+		
+		word_set1_arr[idx].push('both');
+		word_set2_arr[idx].push('둘 다');
+		//END HERE
+		
+		word_set1_arr[idx].push('to take a class');
+		word_set2_arr[idx].push('수업을 듣다');
+		
+		word_set1_arr[idx].push('off course');
+		word_set2_arr[idx].push('그럼요');
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "단어 8과 - a";
+		
+		//01
+		word_set1_arr[idx].push('to taste bland');
+		word_set2_arr[idx].push('싱겁다');
+		
+		word_set1_arr[idx].push('to be young');
+		word_set2_arr[idx].push('젊다');
+		
+		word_set1_arr[idx].push('beef rib stew');
+		word_set2_arr[idx].push('갈비찜');
+		
+		word_set1_arr[idx].push('hot chicken soup with ginseng');
+		word_set2_arr[idx].push('삼계탕');
+		
+		word_set1_arr[idx].push('japanese food');
+		word_set2_arr[idx].push('일식');
+		
+		//06
+		word_set1_arr[idx].push('mixed vegetables and noodles');
+		word_set2_arr[idx].push('잡재');
+		
+		word_set1_arr[idx].push('spring onion pancake');
+		word_set2_arr[idx].push('파전');
+		
+		word_set1_arr[idx].push('spring onion pancake recipe');
+		word_set2_arr[idx].push('파전 만드는 법');
+		
+		word_set1_arr[idx].push('korean food');
+		word_set2_arr[idx].push('한식');
+		
+		word_set1_arr[idx].push('to be greasy');
+		word_set2_arr[idx].push('기름기가 많다');
+		
+		//11
+		word_set1_arr[idx].push('what will you have?');
+		word_set2_arr[idx].push('뭐 드시겠어요');
+		
+		word_set1_arr[idx].push('the atmosphere is nice');
+		word_set2_arr[idx].push('분위기가 좋아요');
+		
+		word_set1_arr[idx].push('the service is good');
+		word_set2_arr[idx].push('서비스가 좋아요');
+		
+		word_set1_arr[idx].push('i will go and check it out');
+		word_set2_arr[idx].push('한번 가 볼게요');
+		
+		word_set1_arr[idx].push('to take out of a bag/pocket');
+		word_set2_arr[idx].push('꺼내다');
+		
+		//16
+		word_set1_arr[idx].push('to put in');
+		word_set2_arr[idx].push('넣다');
+		
+		word_set1_arr[idx].push('to stir-fry');
+		word_set2_arr[idx].push('볶다');
+		
+		word_set1_arr[idx].push('to mix');
+		word_set2_arr[idx].push('섞다');
+		
+		word_set1_arr[idx].push('to chop');
+		word_set2_arr[idx].push('썰다');
+		
+		word_set1_arr[idx].push('to season');
+		word_set2_arr[idx].push('양념하다');
+		//END HERE
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "단어 8과 - b";
+		
+		//01
+		word_set1_arr[idx].push('to be cooked');
+		word_set2_arr[idx].push('익다');
+		
+		word_set1_arr[idx].push('noodles made from potatoes');
+		word_set2_arr[idx].push('당면');
+		
+		word_set1_arr[idx].push('method');
+		word_set2_arr[idx].push('방법');
+		
+		word_set1_arr[idx].push('mushroom');
+		word_set2_arr[idx].push('버섯');
+		
+		word_set1_arr[idx].push('flame');
+		word_set2_arr[idx].push('불');
+		
+		//06
+		word_set1_arr[idx].push('cooking oil');
+		word_set2_arr[idx].push('식용유');
+		
+		word_set1_arr[idx].push('onion');
+		word_set2_arr[idx].push('양파');
+		
+		word_set1_arr[idx].push('ingredients');
+		word_set2_arr[idx].push('재료');
+		
+		word_set1_arr[idx].push('bell pepper');
+		word_set2_arr[idx].push('피망');
+		
+		word_set1_arr[idx].push('to boil water');
+		word_set2_arr[idx].push('물을 끓이다');
+		
+		//11
+		word_set1_arr[idx].push('to put out the flame');
+		word_set2_arr[idx].push('불을 끄다');
+		
+		word_set1_arr[idx].push('to bring');
+		word_set2_arr[idx].push('가져오다');
+		
+		word_set1_arr[idx].push('to start a business');
+		word_set2_arr[idx].push('개업하다');
+		
+		word_set1_arr[idx].push('to cut');
+		word_set2_arr[idx].push('자르다');
+		
+		word_set1_arr[idx].push('neighborhood');
+		word_set2_arr[idx].push('이웃집');
+		
+		//16
+		word_set1_arr[idx].push('seat');
+		word_set2_arr[idx].push('자리');
+		
+		word_set1_arr[idx].push('flower pot');
+		word_set2_arr[idx].push('화분');
+		
+		word_set1_arr[idx].push('to give out rice cake');
+		word_set2_arr[idx].push('떡을 돌리다');
+		
+		word_set1_arr[idx].push('please give me that one');
+		word_set2_arr[idx].push('그걸로 주세요');
+		
+		word_set1_arr[idx].push('enjoy your meal');
+		word_set2_arr[idx].push('맛있게 드세요');
+		//END HERE
+		//21
+		word_set1_arr[idx].push('Naengmyeon is refreshing');
+		word_set2_arr[idx].push('냉면이 시원해요');
+
+		word_set1_arr[idx].push('Here\'s your Mullaengmyeon');
+		word_set2_arr[idx].push('여기 물냉면 나왔어요');
+
+		word_set1_arr[idx].push('Congratulation');
+		word_set2_arr[idx].push('축하 드립니다');
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "단어 9과 - a";
+		
+		//01
+		word_set1_arr[idx].push('to be late');
+		word_set2_arr[idx].push('늦다');
+		
+		word_set1_arr[idx].push('to pass on');
+		word_set2_arr[idx].push('전하다');
+		
+		word_set1_arr[idx].push('to get a job');
+		word_set2_arr[idx].push('취직하다');
+		
+		word_set1_arr[idx].push('right away');
+		word_set2_arr[idx].push('곧');
+		
+		word_set1_arr[idx].push('in a moment');
+		word_set2_arr[idx].push('금방');
+		
+		//06
+		word_set1_arr[idx].push('to come to school');
+		word_set2_arr[idx].push('학교에 나오다');
+		
+		word_set1_arr[idx].push('to take a semester off');
+		word_set2_arr[idx].push('한 학기를 쉬다');
+		
+		word_set1_arr[idx].push('there is a traffic accident');
+		word_set2_arr[idx].push('교통사고가 났어요');
+		
+		word_set1_arr[idx].push('the salary is low');
+		word_set2_arr[idx].push('월급이 적어요');
+		
+		word_set1_arr[idx].push('to continue');
+		word_set2_arr[idx].push('계속하다');
+		
+		//11
+		word_set1_arr[idx].push('to be on sale');
+		word_set2_arr[idx].push('세일하다');
+		
+		word_set1_arr[idx].push('to be bored');
+		word_set2_arr[idx].push('심심하다');
+		
+		word_set1_arr[idx].push('express bus');
+		word_set2_arr[idx].push('고속바스');
+		
+		word_set1_arr[idx].push('bus driver');
+		word_set2_arr[idx].push('버스 기사');
+		
+		word_set1_arr[idx].push('milk');
+		word_set2_arr[idx].push('우유');
+		
+		//16
+		word_set1_arr[idx].push('chocolate');
+		word_set2_arr[idx].push('초콜릿');
+		//END HERE
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "단어 9과 - b";
+		
+		//01
+		word_set1_arr[idx].push('bus terminal');
+		word_set2_arr[idx].push('터미날');
+		
+		word_set1_arr[idx].push('right at that moment');
+		word_set2_arr[idx].push('바로 그 때');
+		
+		word_set1_arr[idx].push('to talk to someone');
+		word_set2_arr[idx].push('(-한테) 말을 걸다');
+		
+		word_set1_arr[idx].push('to hang up the phone');
+		word_set2_arr[idx].push('전화를 끄다');
+		
+		word_set1_arr[idx].push('finally he/she hung up the phone');
+		word_set2_arr[idx].push('드디어 전화를 끊었습니다');
+		
+		//06
+		word_set1_arr[idx].push('could not say anything');
+		word_set2_arr[idx].push('한 마디도 못 했습니다');
+		
+		word_set1_arr[idx].push('to be delayed');
+		word_set2_arr[idx].push('연기되다');
+		
+		word_set1_arr[idx].push('news');
+		word_set2_arr[idx].push('뉴스');
+		
+		word_set1_arr[idx].push('yes please speak');
+		word_set2_arr[idx].push('네 말씀하세요');
+		
+		word_set1_arr[idx].push('pass to him/her a message please');
+		word_set2_arr[idx].push('메모 좀 전해 주시겠어요');
+		
+		//11
+		word_set1_arr[idx].push('what number did you dial?');
+		word_set2_arr[idx].push('몇 번에 거셨어요?');
+		
+		word_set1_arr[idx].push('Isn\'t this Youngho cell phone?');
+		word_set2_arr[idx].push('영호 씨 핸드폰 아니에요?');
+		
+		word_set1_arr[idx].push('It is been a long time');
+		word_set2_arr[idx].push('오랜만이에요');
+		
+		word_set1_arr[idx].push('went out for a little while');
+		word_set2_arr[idx].push('잠깐 나갔는데요');
+		
+		word_set1_arr[idx].push('You have got the wrong number');
+		word_set2_arr[idx].push('전화 잘못 거셨어요');
+		
+		//16
+		word_set1_arr[idx].push('to make a phone call');
+		word_set2_arr[idx].push('전화를 걸다');
+		
+		word_set1_arr[idx].push('to get a call on your cell phone');
+		word_set2_arr[idx].push('핸드폰 받다');
+		
+		//END HERE
+		
+	}
+	
+	/*
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "CHANGE";
+		
+		//01
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//06
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//11
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//16
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		//END HERE
+		
+	}
+	
+	*/
+	
