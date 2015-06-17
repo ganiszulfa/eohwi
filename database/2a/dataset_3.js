eee1b0180295734797e7ecc288d1aaacb0de5083
Ganis Z S <ganiszul.fa@gmail.com>
2015-06-17 16:33:17 +0900

    Create dataset_3.js

---
+++ database/2a/dataset_3.js
@@ -0,0 +1,77 @@
+
+	
+	/*
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "CHANGE";
+		
+		//01
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//06
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//11
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//16
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		//END HERE
+		
+	}
+	
+	*/
+	
