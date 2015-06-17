c53199a2c1e9d88188d8f9a9058032ca0a93bcd5
Ganis Z S <ganiszul.fa@gmail.com>
2015-06-17 16:37:15 +0900

    Update dataset_2.js

--- database/2a/dataset_2.js
+++ database/2a/dataset_2.js
@@ -1,271 +1,272 @@
 
+
 if (true) {
 		idx++;
-		explain_set[idx] = "ë¬¸ë²• - part A";
+		explain_set[idx] = "문법 - part A";
 		
 		//01
 		word_set1_arr[idx].push('(i) close the door (polite)');
-		word_set2_arr[idx].push('ë¬¸ì„ ë‹«ìœ¼ì„¸ìš”');
+		word_set2_arr[idx].push('문을 닫으세요');
 		
 		word_set1_arr[idx].push('(i) received the phone call (polite)');
-		word_set2_arr[idx].push('ì „í™”ë¥¼ ë°›ìœ¼ì…¨ìš”');
+		word_set2_arr[idx].push('전화를 받으셨요');
 		
 		word_set1_arr[idx].push('(i) will go to the market (polite)');
-		word_set2_arr[idx].push('ì‹œìž¥ì— ê°€ì‹¤ ê±°ì˜ˆìš”');
+		word_set2_arr[idx].push('시장에 가실 거예요');
 		
 		word_set1_arr[idx].push('(i) listen to the music (polite)');
-		word_set2_arr[idx].push('ìŒì•…ì„ ë“¤ìœ¼ì„¸ìš”');
+		word_set2_arr[idx].push('음악을 들으세요');
 		
 		word_set1_arr[idx].push('(i) live in dormitory (polite)');
-		word_set2_arr[idx].push('ê¸°ìˆ™ì‚¬ì—ì„œ ì‚¬ì„¸ìš”');
+		word_set2_arr[idx].push('기숙사에서 사세요');
 		
 		//06
 		word_set1_arr[idx].push('to be exist (polite)');
-		word_set2_arr[idx].push('ê³„ì‹œë‹¤');
+		word_set2_arr[idx].push('계시다');
 		
 		word_set1_arr[idx].push('to sleep (polite)');
-		word_set2_arr[idx].push('ì£¼ë¬´ì‹œë‹¤');
+		word_set2_arr[idx].push('주무시다');
 		
 		word_set1_arr[idx].push('(i) will buy for you (polite)');
-		word_set2_arr[idx].push('ì‚¬ ë“œë¦´ê²Œìš”');
+		word_set2_arr[idx].push('사 드릴게요');
 		
 		word_set1_arr[idx].push('(i) will teach for you (polite)');
-		word_set2_arr[idx].push('ê°€ë¥´ì³ ë“œë¦´ê²Œìš”');
+		word_set2_arr[idx].push('가르쳐 드릴게요');
 		
 		word_set1_arr[idx].push('(i) will write for you (polite)');
-		word_set2_arr[idx].push('ì¨ ë“œë¦´ê²Œìš”');
+		word_set2_arr[idx].push('써 드릴게요');
 		
 		//11
 		word_set1_arr[idx].push('(it is) a book');
-		word_set2_arr[idx].push('ì±…ì´ì—ìš”');
+		word_set2_arr[idx].push('책이에요');
 		
 		word_set1_arr[idx].push('(i am) not an american');
-		word_set2_arr[idx].push('ë¯¸êµ­ ì‚¬ëžŒ ì•„ë‹ˆì—ìš”');
+		word_set2_arr[idx].push('미국 사람 아니에요');
 		
 		word_set1_arr[idx].push('(i am) watching a movie');
-		word_set2_arr[idx].push('ì˜í™”ë¥¼ ë³´ê³ ìžˆì–´ìš”');
+		word_set2_arr[idx].push('영화를 보고있어요');
 		
 		word_set1_arr[idx].push('(i am) listening to a lecture');
-		word_set2_arr[idx].push('ê°•ì˜ë¥¼ ë“£ê³  ìžˆì–´ìš”');
+		word_set2_arr[idx].push('강의를 듣고 있어요');
 		
 		word_set1_arr[idx].push('(i) take a shower after (i) exercise');
-		word_set2_arr[idx].push('ìš´ë™ì„ í•œ ë‹¤ìŒì— ì‚¬ì›Œë¥¼ í•´ìš”');
+		word_set2_arr[idx].push('운동을 한 다음에 사워를 해요');
 		
 		//16
 		word_set1_arr[idx].push('(i) ate after (i) woke up');
-		word_set2_arr[idx].push('ì¼ì–´ë‚œ ë‹¤ìŒì— ë¨¹ì—ˆì–´ìš”');
+		word_set2_arr[idx].push('일어난 다음에 먹었어요');
 		
 		word_set1_arr[idx].push('(i) will sleep after (i) dance');
-		word_set2_arr[idx].push('ì¶¤ì¤€ ë‹¤ìŒì— ìž˜ ê±°ì˜ˆìš”');
+		word_set2_arr[idx].push('춤준 다음에 잘 거예요');
 		
 		word_set1_arr[idx].push('(i) buy a cake for a gift');
-		word_set2_arr[idx].push('ì„ ë¬¼í•˜ë ¤ê³  ì¼€ì´í¬ë¥¼ ì‚¬ìš”');
+		word_set2_arr[idx].push('선물하려고 케이크를 사요');
 		
 		word_set1_arr[idx].push('(i) went to school in order to study');
-		word_set2_arr[idx].push('ê³µë¶€í•˜ë ¤ê³  í•™êµì— ê°”ì–´ìš”');
+		word_set2_arr[idx].push('공부하려고 학교에 갔어요');
 		
 		word_set1_arr[idx].push('(i) bought the CD in order to listen to a song');
-		word_set2_arr[idx].push('ë…¸ëž˜ë¥¼ ë“¤ìœ¼ë ¤ê³  CDë¥¼ ìƒ€ì–´ìš”');
+		word_set2_arr[idx].push('노래를 들으려고 CD를 샀어요');
 		//END HERE
 		
 	}
 	
 if (true) {
 		idx++;
-		explain_set[idx] = "ë¬¸ë²• - part B";
+		explain_set[idx] = "문법 - part B";
 		
 		//01
 		word_set1_arr[idx].push('When I lived in China I traveled alot');
-		word_set2_arr[idx].push('ì¤‘êµ­ì—ì„œ ì‚´ ë•Œ ì—¬í–‰ì„ ë§Žì´ í–ˆì–´ìš”');
+		word_set2_arr[idx].push('중국에서 살 때 여행을 많이 했어요');
 		
 		word_set1_arr[idx].push('When I listen to the music my feeling is happy');
-		word_set2_arr[idx].push('ìŒì•…ì„ ë“¤ì„ ë•Œ ê¸°ë¶„ì´ ì¢‹ì•„ìš”');
+		word_set2_arr[idx].push('음악을 들을 때 기분이 좋아요');
 		
 		word_set1_arr[idx].push('when the weather is cold don\'t go outside');
-		word_set2_arr[idx].push('ë‚ ì”¨ê°€ ì¶”ìš¸ ë•Œ ë°•ì— ë‚˜ê°€ì§€ ë§ˆì„¸ìš”');
+		word_set2_arr[idx].push('날씨가 추울 때 박에 나가지 마세요');
 		
 		word_set1_arr[idx].push('what do you want to eat at dinner time?');
-		word_set2_arr[idx].push('ì €ë… ë•Œ ë­ ë¨¹ì–´ìš”?');
+		word_set2_arr[idx].push('저녁 때 뭐 먹어요?');
 		
 		word_set1_arr[idx].push('before (i) eat (i) clean (my) hands');
-		word_set2_arr[idx].push('ë¨¹ê¸° ì „ì— ì†ì„ ì”»ì–´ìš”');
+		word_set2_arr[idx].push('먹기 전에 손을 씻어요');
 		
 		//06
 		word_set1_arr[idx].push('before (i) go (i) finished the homework');
-		word_set2_arr[idx].push('ê°€ê¸° ì „ì— ìˆ™ì œë¥¼ ëëƒˆì–´ìš”');
+		word_set2_arr[idx].push('가기 전에 숙제를 끝냈어요');
 		
 		word_set1_arr[idx].push('before (i) go (i) will finish the homework');
-		word_set2_arr[idx].push('ê°€ê¸° ì „ì— ìˆ™ì œë¥¼ ëë‚¼ ê±°ì˜ˆìš”');
+		word_set2_arr[idx].push('가기 전에 숙제를 끝낼 거예요');
 		
 		word_set1_arr[idx].push('(i) meet friend and so (we) take lunch together');
-		word_set2_arr[idx].push('ì¹œêµ¬ë¥¼ ë§Œë‚˜ì„œ ê°™ì´ ì ì‹¬ ë¨¹ì–´ìš”');
+		word_set2_arr[idx].push('친구를 만나서 같이 점심 먹어요');
 		
 		word_set1_arr[idx].push('(i) met friend and so (we) took lunch together');
-		word_set2_arr[idx].push('ì¹œêµ¬ë¥¼ ë§Œë‚˜ì„œ ê°™ì´ ì ì‹¬ ë¨¹ì—ˆì–´ìš”');
+		word_set2_arr[idx].push('친구를 만나서 같이 점심 먹었어요');
 		
 		word_set1_arr[idx].push('(i) will meet friend and so (we) will take lunch together');
-		word_set2_arr[idx].push('ì¹œêµ¬ë¥¼ ë§Œë‚˜ì„œ ê°™ì´ ì ì‹¬ ë¨¹ì„ ê±°ì˜ˆìš”');
+		word_set2_arr[idx].push('친구를 만나서 같이 점심 먹을 거예요');
 		
 		//11
 		word_set1_arr[idx].push('if (i) read a book (my) eyes hurt');
-		word_set2_arr[idx].push('ì±…ì„ ì½ìœ¼ë©´ ëˆˆì´ ì•„íŒŒìš”');
+		word_set2_arr[idx].push('책을 읽으면 눈이 아파요');
 		
 		word_set1_arr[idx].push('if (it is) expensive (i) won\'t buy (it)');
-		word_set2_arr[idx].push('ë¹„ì‹¸ë©´ ì‚¬ì§€ ì•Šì„ ê±°ì˜ˆìš”');
+		word_set2_arr[idx].push('비싸면 사지 않을 거예요');
 		
 		word_set1_arr[idx].push('if (it is) cold close the door ((polite)');
-		word_set2_arr[idx].push('ì¶”ìš°ë©´ ë¬¸ì„ ë‹«ìœ¼ì„¸ìš”');
+		word_set2_arr[idx].push('추우면 문을 닫으세요');
 		
 		word_set1_arr[idx].push('if (i) attended the company (i) would had met many people');
-		word_set2_arr[idx].push('íšŒì‚¬ì— ë‹¤ë‹ ë•Œ ë§Žì€ ì‚¬ëžŒë“¤ì„ ë§Œë‚¬ì–´ìš”');
+		word_set2_arr[idx].push('회사에 다닐 때 많은 사람들을 만났어요');
 		
 		word_set1_arr[idx].push('where if (i) buy (it) will be cheap?');
-		word_set2_arr[idx].push('ì–´ë””ì—ì„œ ì‚¬ë©´ ìŒ€ê¹Œìš”?');
+		word_set2_arr[idx].push('어디에서 사면 쌀까요?');
 		
 		//16
 		word_set1_arr[idx].push('do (you) think it will close?');
-		word_set2_arr[idx].push('ë‹«ì„ê¹Œìš”?');
+		word_set2_arr[idx].push('닫을까요?');
 		
 		word_set1_arr[idx].push('how do (you) think the weather will be?');
-		word_set2_arr[idx].push('ë‚ ì”¨ê°€ ì–´ë–¨ê¹Œìš”?');
+		word_set2_arr[idx].push('날씨가 어떨까요?');
 		
 		word_set1_arr[idx].push('(i) will not listen to the radio');
-		word_set2_arr[idx].push('ë¼ë””ì˜¤ë¥¼ ì•ˆ ë“¤ì„ ê±°ì˜ˆìš”');
+		word_set2_arr[idx].push('라디오를 안 들을 거예요');
 		
 		word_set1_arr[idx].push('the test will be difficult');
-		word_set2_arr[idx].push('ì‹œí—˜ì´ ì–´ë ¤ìš¸ê±°ì˜ˆìš”');
+		word_set2_arr[idx].push('시험이 어려울거예요');
 		
 		word_set1_arr[idx].push('after (i) watched a movie (i) started studying');
-		word_set2_arr[idx].push('ì˜í™”ë¥¼ ë³´ë‹¤ê°€ ê³µë¶€í—¸ì–´ìš”');
+		word_set2_arr[idx].push('영화를 보다가 공부헸어요');
 		//END HERE
 		
 		word_set1_arr[idx].push('after (i) watched a movie (i) will study');
-		word_set2_arr[idx].push('ì˜í™”ë¥¼ ë³´ë‹¤ê°€ ê³µë¶€í•  ê±°ì˜ˆìš”');
+		word_set2_arr[idx].push('영화를 보다가 공부할 거예요');
 		
 	}
 	
 if (true) {
 		idx++;
-		explain_set[idx] = "ë¬¸ë²• - part C";
+		explain_set[idx] = "문법 - part C";
 		
 		//01
 		word_set1_arr[idx].push('finding a good homestay is difficult');
-		word_set2_arr[idx].push('ì¢‹ì€ í•˜ìˆ™ì§‘ì„ ì°¾ëŠ” ê²Œ ì–´ë ¤ì›Œìš”');
+		word_set2_arr[idx].push('좋은 하숙집을 찾는 게 어려워요');
 		
 		word_set1_arr[idx].push('(i) really like kids playing outside');
-		word_set2_arr[idx].push('ì•„ì´ë“¤ì€ ë°–ì—ì„œ ë…¸ëŠ” ê±° ì•„ì£¼ ì¢‹ì•„í•´ìš”');
+		word_set2_arr[idx].push('아이들은 밖에서 노는 거 아주 좋아해요');
 		
 		word_set1_arr[idx].push('(i) promise to play with (my) friends');
-		word_set2_arr[idx].push('ì¹œêµ¬ë“¤í•˜ê³  ë†€ê¸°ë¡œ í–ˆì–´ìš”');
+		word_set2_arr[idx].push('친구들하고 놀기로 했어요');
 		
 		word_set1_arr[idx].push('(i) promise to watch football match with (my) dad');
-		word_set2_arr[idx].push('ì•„ë¹ ëž‘  ì¶•êµ¬ ê²½ê¸°ë¥¼ ë³´ê¸°ë¡œ í–ˆì–´ìš”');
+		word_set2_arr[idx].push('아빠랑  축구 경기를 보기로 했어요');
 		
 		word_set1_arr[idx].push('since (i\'m) busy today; how about meeting at weekend?');
-		word_set2_arr[idx].push('ì˜¤ëŠ˜ì€ ë°”ì˜ë‹ˆê¹Œ ì£¼ë§ì— ë§Œë‚˜ëŠ” ê²Œ ì–´ë•Œìš”?');
+		word_set2_arr[idx].push('오늘은 바쁘니까 주말에 만나는 게 어때요?');
 		
 		//06
 		word_set1_arr[idx].push('since the traffic is bad now; take the subway');
-		word_set2_arr[idx].push('ê¸¸ì´ ë§‰í•˜ë‹ˆê¹Œ ì§€í•˜ì² ì„ íƒ€ì„¸ìš”');
+		word_set2_arr[idx].push('길이 막하니까 지하철을 타세요');
 		
 		word_set1_arr[idx].push('since It\'s cold outside; wear thick clothes');
-		word_set2_arr[idx].push('ë°–ì´ ì¶”ìš°ë‹ˆê¹Œ ë‘êº¼ìš´ ì˜·ì„ ìž…ìœ¼ì„¸ìš”');
+		word_set2_arr[idx].push('밖이 추우니까 두꺼운 옷을 입으세요');
 		
 		word_set1_arr[idx].push('since it\'s hot; let\'s open the door (polite)');
-		word_set2_arr[idx].push('ë”ìš°ë‹ˆê¹Œ ë¬¸ì„ ì—½ì‹œë‹¤');
+		word_set2_arr[idx].push('더우니까 문을 엽시다');
 		
 		word_set1_arr[idx].push('Let\'s meet in front of library (polite)');
-		word_set2_arr[idx].push('ë„ì„œê´€ ì•žì—ì„œ ë§Œë‚©ì‹œë‹¤');
+		word_set2_arr[idx].push('도서관 앞에서 만납시다');
 		
 		word_set1_arr[idx].push('how about eating lunch together?');
-		word_set2_arr[idx].push('ì ì‹¬ ë¨¹ëŠ” ê²Œ ì–´ë•Œìš”?');
+		word_set2_arr[idx].push('점심 먹는 게 어때요?');
 		
 		//11
 		word_set1_arr[idx].push('(you) can go out early');
-		word_set2_arr[idx].push('ì¼ì° ë‚˜ê°€ë„ ë¼ìš”');
+		word_set2_arr[idx].push('일찍 나가도 돼요');
 		
 		word_set1_arr[idx].push('can (i) listen to the advertisement?');
-		word_set2_arr[idx].push('ê´‘ê³ ë¥¼ ë“¤ì–´ë„ ë¼ìš”?');
+		word_set2_arr[idx].push('광고를 들어도 돼요?');
 		
 		word_set1_arr[idx].push('can (i) use the chopsticks?');
-		word_set2_arr[idx].push('ì “ê°€ë½ ì¨ë„ ë¼ìš”?');
+		word_set2_arr[idx].push('젓가락 써도 돼요?');
 		
 		word_set1_arr[idx].push('(you) can\'t sit here');
-		word_set2_arr[idx].push('ì—¬ê¸°ì—ì„œ ì•‰ìœ¼ë©´ ì•ˆ ë¼ìš”');
+		word_set2_arr[idx].push('여기에서 앉으면 안 돼요');
 		
 		word_set1_arr[idx].push('since (i) have a test tomorrow; (i) can\'t play');
-		word_set2_arr[idx].push('ë‚´ì¼ ì‹œí—˜ì´ë‹ˆê¹Œ ì˜¤ëŠ˜ ë†€ë©´ ì•ˆ ë¼ìš”');
+		word_set2_arr[idx].push('내일 시험이니까 오늘 놀면 안 돼요');
 		
 		//16
 		word_set1_arr[idx].push('(you) can\'t listen to the music');
-		word_set2_arr[idx].push('ìŒì•…ì„ ë“¤ìœ¼ë©´ ì•ˆ ë¼ìš”');
+		word_set2_arr[idx].push('음악을 들으면 안 돼요');
 		
 		word_set1_arr[idx].push('but it\'s small');
-		word_set2_arr[idx].push('ìž‘ì€ë°ìš”');
+		word_set2_arr[idx].push('작은데요');
 		
 		word_set1_arr[idx].push('but (i\'m) little bit busy');
-		word_set2_arr[idx].push('ì¢€ ë°”ìœë°ìš”');
+		word_set2_arr[idx].push('좀 바쁜데요');
 		
 		word_set1_arr[idx].push('but now is raining');
-		word_set2_arr[idx].push('ì§€ê¸ˆ ë¹„ê°€ ì˜¤ëŠ”ëŒ€ìš”');
+		word_set2_arr[idx].push('지금 비가 오는대요');
 		
 		word_set1_arr[idx].push('but (i) have another appointment');
-		word_set2_arr[idx].push('ë‹¤ë¥¸ ì•½ì†ì´ ìžˆëŠ”ë°ìš”');
+		word_set2_arr[idx].push('다른 약속이 있는데요');
 		//END HERE
 		
 		word_set1_arr[idx].push('but it is meeting time');
-		word_set2_arr[idx].push('íšŒì˜ ì‹œê°„ì¸ë°ìš”');
+		word_set2_arr[idx].push('회의 시간인데요');
 		
 		word_set1_arr[idx].push('but (i) was busy');
-		word_set2_arr[idx].push('ë°”ë¹´ëŠ”ë°ìš”');
+		word_set2_arr[idx].push('바빴는데요');
 		
 		word_set1_arr[idx].push('but it is my friend');
-		word_set2_arr[idx].push('ì œ ì¹œêµ°ë°ìš”');
+		word_set2_arr[idx].push('제 친군데요');
 		
 	}
 	
 if (true) {
 		idx++;
-		explain_set[idx] = "ë¬¸ë²• - part D";
+		explain_set[idx] = "문법 - part D";
 		
 		//01
 		word_set1_arr[idx].push('Can I take your bag?');
-		word_set2_arr[idx].push('ê°€ë°©ì„ ë“¤ì–´ ë“œë¦´ê¹Œìš”?');
+		word_set2_arr[idx].push('가방을 들어 드릴까요?');
 		
 		word_set1_arr[idx].push('How can i help you?');
-		word_set2_arr[idx].push('ë­ ë„ì™€ ë“œë¦´ê¹Œìš”?');
+		word_set2_arr[idx].push('뭐 도와 드릴까요?');
 		
 		word_set1_arr[idx].push('Can I open the door for you?');
-		word_set2_arr[idx].push('ë¬¸ ì—´ì–´ ë“œë¦´ê¹Œìš”');
+		word_set2_arr[idx].push('문 열어 드릴까요');
 		
 		word_set1_arr[idx].push('Could you close the door for me?');
-		word_set2_arr[idx].push('ë¬¸ ì¢€ ë‹«ì•„ ì£¼ì‹œê² ì–´ìš”?');
+		word_set2_arr[idx].push('문 좀 닫아 주시겠어요?');
 		
 		word_set1_arr[idx].push('Could you teach me German?');
-		word_set2_arr[idx].push('ë…ì¼ì–´ ì¢€ ê°€ë¥´ì³ ì£¼ì‹œê² ì–´ìš”?');
+		word_set2_arr[idx].push('독일어 좀 가르쳐 주시겠어요?');
 		
 		//06
 		word_set1_arr[idx].push('Could I borrow your pen?');
-		word_set2_arr[idx].push('íŽœ ì¢€ ë¹Œë ¤ ì£¼ì‹œê² ì–´ìš”?');
+		word_set2_arr[idx].push('펜 좀 빌려 주시겠어요?');
 		
 		word_set1_arr[idx].push('Minhye said that her feeling is good');
-		word_set2_arr[idx].push('ë¯¼í˜œ ì”¨ê°€ ê¸°ë¶„ì´ ì¢‹ë‹¤ê³  í–ˆì–´ìš”');
+		word_set2_arr[idx].push('민혜 씨가 기분이 좋다고 했어요');
 		
 		word_set1_arr[idx].push('Jeny say that lately she is not busy');
-		word_set2_arr[idx].push('ì œë‹ˆ ì”¨ê°€ ìš”ì¦˜ ë°”ì˜ì§€ ì•Šë‹¤ê³  í–ˆì–´ìš”');
+		word_set2_arr[idx].push('제니 씨가 요즘 바쁘지 않다고 했어요');
 		
 		word_set1_arr[idx].push('Kim Yura said she is healthy');
-		word_set2_arr[idx].push('ê¹€ì¹˜ê°€ ê±´ê°•ì— ì¢‹ë‹¤ê³  í•´ìš”');
+		word_set2_arr[idx].push('김치가 건강에 좋다고 해요');
 		
 		word_set1_arr[idx].push('Andy said he read the book ');
-		word_set2_arr[idx].push('ì•¤ë”” ì”¨ê°€ ì±…ì„ ì½ëŠ”ë‹¤ê³  í–ˆì–´ìš”');
+		word_set2_arr[idx].push('앤디 씨가 책을 읽는다고 했어요');
 		
 		//11
 		word_set1_arr[idx].push('Sora said that right now is raining');
-		word_set2_arr[idx].push('ì†Œë¼ ì”¨ê°€ ì§€ê¸ˆ ë¹„ê°€ ì˜¨ë‹¤ê³  í–ˆì–´ìš”');
+		word_set2_arr[idx].push('소라 씨가 지금 비가 온다고 했어요');
 		
 		word_set1_arr[idx].push('Irina said that lately she has not been eating well.');
-		word_set2_arr[idx].push('ì´ë¦¬ë‚˜ ì”¨ê°€ ìš”ì¦˜ ë°¥ì„ ìž˜ ë¨¹ì§€ ì•ŠëŠ”ë‹¤ê³  í–ˆì–´ìš”');
+		word_set2_arr[idx].push('이리나 씨가 요즘 밥을 잘 먹지 않는다고 했어요');
 		
 		//END HERE
 		
