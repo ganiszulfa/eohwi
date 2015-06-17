f1915803e2d6db07bdd95b7ceb9cbd27d4a4c9bf
Ganis Z S <ganiszul.fa@gmail.com>
2015-06-17 16:32:51 +0900

    Create dataset_2.js

---
+++ database/2a/dataset_2.js
@@ -0,0 +1,349 @@
+
+if (true) {
+		idx++;
+		explain_set[idx] = "ë¬¸ë²• - part A";
+		
+		//01
+		word_set1_arr[idx].push('(i) close the door (polite)');
+		word_set2_arr[idx].push('ë¬¸ì„ ë‹«ìœ¼ì„¸ìš”');
+		
+		word_set1_arr[idx].push('(i) received the phone call (polite)');
+		word_set2_arr[idx].push('ì „í™”ë¥¼ ë°›ìœ¼ì…¨ìš”');
+		
+		word_set1_arr[idx].push('(i) will go to the market (polite)');
+		word_set2_arr[idx].push('ì‹œìž¥ì— ê°€ì‹¤ ê±°ì˜ˆìš”');
+		
+		word_set1_arr[idx].push('(i) listen to the music (polite)');
+		word_set2_arr[idx].push('ìŒì•…ì„ ë“¤ìœ¼ì„¸ìš”');
+		
+		word_set1_arr[idx].push('(i) live in dormitory (polite)');
+		word_set2_arr[idx].push('ê¸°ìˆ™ì‚¬ì—ì„œ ì‚¬ì„¸ìš”');
+		
+		//06
+		word_set1_arr[idx].push('to be exist (polite)');
+		word_set2_arr[idx].push('ê³„ì‹œë‹¤');
+		
+		word_set1_arr[idx].push('to sleep (polite)');
+		word_set2_arr[idx].push('ì£¼ë¬´ì‹œë‹¤');
+		
+		word_set1_arr[idx].push('(i) will buy for you (polite)');
+		word_set2_arr[idx].push('ì‚¬ ë“œë¦´ê²Œìš”');
+		
+		word_set1_arr[idx].push('(i) will teach for you (polite)');
+		word_set2_arr[idx].push('ê°€ë¥´ì³ ë“œë¦´ê²Œìš”');
+		
+		word_set1_arr[idx].push('(i) will write for you (polite)');
+		word_set2_arr[idx].push('ì¨ ë“œë¦´ê²Œìš”');
+		
+		//11
+		word_set1_arr[idx].push('(it is) a book');
+		word_set2_arr[idx].push('ì±…ì´ì—ìš”');
+		
+		word_set1_arr[idx].push('(i am) not an american');
+		word_set2_arr[idx].push('ë¯¸êµ­ ì‚¬ëžŒ ì•„ë‹ˆì—ìš”');
+		
+		word_set1_arr[idx].push('(i am) watching a movie');
+		word_set2_arr[idx].push('ì˜í™”ë¥¼ ë³´ê³ ìžˆì–´ìš”');
+		
+		word_set1_arr[idx].push('(i am) listening to a lecture');
+		word_set2_arr[idx].push('ê°•ì˜ë¥¼ ë“£ê³  ìžˆì–´ìš”');
+		
+		word_set1_arr[idx].push('(i) take a shower after (i) exercise');
+		word_set2_arr[idx].push('ìš´ë™ì„ í•œ ë‹¤ìŒì— ì‚¬ì›Œë¥¼ í•´ìš”');
+		
+		//16
+		word_set1_arr[idx].push('(i) ate after (i) woke up');
+		word_set2_arr[idx].push('ì¼ì–´ë‚œ ë‹¤ìŒì— ë¨¹ì—ˆì–´ìš”');
+		
+		word_set1_arr[idx].push('(i) will sleep after (i) dance');
+		word_set2_arr[idx].push('ì¶¤ì¤€ ë‹¤ìŒì— ìž˜ ê±°ì˜ˆìš”');
+		
+		word_set1_arr[idx].push('(i) buy a cake for a gift');
+		word_set2_arr[idx].push('ì„ ë¬¼í•˜ë ¤ê³  ì¼€ì´í¬ë¥¼ ì‚¬ìš”');
+		
+		word_set1_arr[idx].push('(i) went to school in order to study');
+		word_set2_arr[idx].push('ê³µë¶€í•˜ë ¤ê³  í•™êµì— ê°”ì–´ìš”');
+		
+		word_set1_arr[idx].push('(i) bought the CD in order to listen to a song');
+		word_set2_arr[idx].push('ë…¸ëž˜ë¥¼ ë“¤ìœ¼ë ¤ê³  CDë¥¼ ìƒ€ì–´ìš”');
+		//END HERE
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "ë¬¸ë²• - part B";
+		
+		//01
+		word_set1_arr[idx].push('When I lived in China I traveled alot');
+		word_set2_arr[idx].push('ì¤‘êµ­ì—ì„œ ì‚´ ë•Œ ì—¬í–‰ì„ ë§Žì´ í–ˆì–´ìš”');
+		
+		word_set1_arr[idx].push('When I listen to the music my feeling is happy');
+		word_set2_arr[idx].push('ìŒì•…ì„ ë“¤ì„ ë•Œ ê¸°ë¶„ì´ ì¢‹ì•„ìš”');
+		
+		word_set1_arr[idx].push('when the weather is cold don\'t go outside');
+		word_set2_arr[idx].push('ë‚ ì”¨ê°€ ì¶”ìš¸ ë•Œ ë°•ì— ë‚˜ê°€ì§€ ë§ˆì„¸ìš”');
+		
+		word_set1_arr[idx].push('what do you want to eat at dinner time?');
+		word_set2_arr[idx].push('ì €ë… ë•Œ ë­ ë¨¹ì–´ìš”?');
+		
+		word_set1_arr[idx].push('before (i) eat (i) clean (my) hands');
+		word_set2_arr[idx].push('ë¨¹ê¸° ì „ì— ì†ì„ ì”»ì–´ìš”');
+		
+		//06
+		word_set1_arr[idx].push('before (i) go (i) finished the homework');
+		word_set2_arr[idx].push('ê°€ê¸° ì „ì— ìˆ™ì œë¥¼ ëëƒˆì–´ìš”');
+		
+		word_set1_arr[idx].push('before (i) go (i) will finish the homework');
+		word_set2_arr[idx].push('ê°€ê¸° ì „ì— ìˆ™ì œë¥¼ ëë‚¼ ê±°ì˜ˆìš”');
+		
+		word_set1_arr[idx].push('(i) meet friend and so (we) take lunch together');
+		word_set2_arr[idx].push('ì¹œêµ¬ë¥¼ ë§Œë‚˜ì„œ ê°™ì´ ì ì‹¬ ë¨¹ì–´ìš”');
+		
+		word_set1_arr[idx].push('(i) met friend and so (we) took lunch together');
+		word_set2_arr[idx].push('ì¹œêµ¬ë¥¼ ë§Œë‚˜ì„œ ê°™ì´ ì ì‹¬ ë¨¹ì—ˆì–´ìš”');
+		
+		word_set1_arr[idx].push('(i) will meet friend and so (we) will take lunch together');
+		word_set2_arr[idx].push('ì¹œêµ¬ë¥¼ ë§Œë‚˜ì„œ ê°™ì´ ì ì‹¬ ë¨¹ì„ ê±°ì˜ˆìš”');
+		
+		//11
+		word_set1_arr[idx].push('if (i) read a book (my) eyes hurt');
+		word_set2_arr[idx].push('ì±…ì„ ì½ìœ¼ë©´ ëˆˆì´ ì•„íŒŒìš”');
+		
+		word_set1_arr[idx].push('if (it is) expensive (i) won\'t buy (it)');
+		word_set2_arr[idx].push('ë¹„ì‹¸ë©´ ì‚¬ì§€ ì•Šì„ ê±°ì˜ˆìš”');
+		
+		word_set1_arr[idx].push('if (it is) cold close the door ((polite)');
+		word_set2_arr[idx].push('ì¶”ìš°ë©´ ë¬¸ì„ ë‹«ìœ¼ì„¸ìš”');
+		
+		word_set1_arr[idx].push('if (i) attended the company (i) would had met many people');
+		word_set2_arr[idx].push('íšŒì‚¬ì— ë‹¤ë‹ ë•Œ ë§Žì€ ì‚¬ëžŒë“¤ì„ ë§Œë‚¬ì–´ìš”');
+		
+		word_set1_arr[idx].push('where if (i) buy (it) will be cheap?');
+		word_set2_arr[idx].push('ì–´ë””ì—ì„œ ì‚¬ë©´ ìŒ€ê¹Œìš”?');
+		
+		//16
+		word_set1_arr[idx].push('do (you) think it will close?');
+		word_set2_arr[idx].push('ë‹«ì„ê¹Œìš”?');
+		
+		word_set1_arr[idx].push('how do (you) think the weather will be?');
+		word_set2_arr[idx].push('ë‚ ì”¨ê°€ ì–´ë–¨ê¹Œìš”?');
+		
+		word_set1_arr[idx].push('(i) will not listen to the radio');
+		word_set2_arr[idx].push('ë¼ë””ì˜¤ë¥¼ ì•ˆ ë“¤ì„ ê±°ì˜ˆìš”');
+		
+		word_set1_arr[idx].push('the test will be difficult');
+		word_set2_arr[idx].push('ì‹œí—˜ì´ ì–´ë ¤ìš¸ê±°ì˜ˆìš”');
+		
+		word_set1_arr[idx].push('after (i) watched a movie (i) started studying');
+		word_set2_arr[idx].push('ì˜í™”ë¥¼ ë³´ë‹¤ê°€ ê³µë¶€í—¸ì–´ìš”');
+		//END HERE
+		
+		word_set1_arr[idx].push('after (i) watched a movie (i) will study');
+		word_set2_arr[idx].push('ì˜í™”ë¥¼ ë³´ë‹¤ê°€ ê³µë¶€í•  ê±°ì˜ˆìš”');
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "ë¬¸ë²• - part C";
+		
+		//01
+		word_set1_arr[idx].push('finding a good homestay is difficult');
+		word_set2_arr[idx].push('ì¢‹ì€ í•˜ìˆ™ì§‘ì„ ì°¾ëŠ” ê²Œ ì–´ë ¤ì›Œìš”');
+		
+		word_set1_arr[idx].push('(i) really like kids playing outside');
+		word_set2_arr[idx].push('ì•„ì´ë“¤ì€ ë°–ì—ì„œ ë…¸ëŠ” ê±° ì•„ì£¼ ì¢‹ì•„í•´ìš”');
+		
+		word_set1_arr[idx].push('(i) promise to play with (my) friends');
+		word_set2_arr[idx].push('ì¹œêµ¬ë“¤í•˜ê³  ë†€ê¸°ë¡œ í–ˆì–´ìš”');
+		
+		word_set1_arr[idx].push('(i) promise to watch football match with (my) dad');
+		word_set2_arr[idx].push('ì•„ë¹ ëž‘  ì¶•êµ¬ ê²½ê¸°ë¥¼ ë³´ê¸°ë¡œ í–ˆì–´ìš”');
+		
+		word_set1_arr[idx].push('since (i\'m) busy today; how about meeting at weekend?');
+		word_set2_arr[idx].push('ì˜¤ëŠ˜ì€ ë°”ì˜ë‹ˆê¹Œ ì£¼ë§ì— ë§Œë‚˜ëŠ” ê²Œ ì–´ë•Œìš”?');
+		
+		//06
+		word_set1_arr[idx].push('since the traffic is bad now; take the subway');
+		word_set2_arr[idx].push('ê¸¸ì´ ë§‰í•˜ë‹ˆê¹Œ ì§€í•˜ì² ì„ íƒ€ì„¸ìš”');
+		
+		word_set1_arr[idx].push('since It\'s cold outside; wear thick clothes');
+		word_set2_arr[idx].push('ë°–ì´ ì¶”ìš°ë‹ˆê¹Œ ë‘êº¼ìš´ ì˜·ì„ ìž…ìœ¼ì„¸ìš”');
+		
+		word_set1_arr[idx].push('since it\'s hot; let\'s open the door (polite)');
+		word_set2_arr[idx].push('ë”ìš°ë‹ˆê¹Œ ë¬¸ì„ ì—½ì‹œë‹¤');
+		
+		word_set1_arr[idx].push('Let\'s meet in front of library (polite)');
+		word_set2_arr[idx].push('ë„ì„œê´€ ì•žì—ì„œ ë§Œë‚©ì‹œë‹¤');
+		
+		word_set1_arr[idx].push('how about eating lunch together?');
+		word_set2_arr[idx].push('ì ì‹¬ ë¨¹ëŠ” ê²Œ ì–´ë•Œìš”?');
+		
+		//11
+		word_set1_arr[idx].push('(you) can go out early');
+		word_set2_arr[idx].push('ì¼ì° ë‚˜ê°€ë„ ë¼ìš”');
+		
+		word_set1_arr[idx].push('can (i) listen to the advertisement?');
+		word_set2_arr[idx].push('ê´‘ê³ ë¥¼ ë“¤ì–´ë„ ë¼ìš”?');
+		
+		word_set1_arr[idx].push('can (i) use the chopsticks?');
+		word_set2_arr[idx].push('ì “ê°€ë½ ì¨ë„ ë¼ìš”?');
+		
+		word_set1_arr[idx].push('(you) can\'t sit here');
+		word_set2_arr[idx].push('ì—¬ê¸°ì—ì„œ ì•‰ìœ¼ë©´ ì•ˆ ë¼ìš”');
+		
+		word_set1_arr[idx].push('since (i) have a test tomorrow; (i) can\'t play');
+		word_set2_arr[idx].push('ë‚´ì¼ ì‹œí—˜ì´ë‹ˆê¹Œ ì˜¤ëŠ˜ ë†€ë©´ ì•ˆ ë¼ìš”');
+		
+		//16
+		word_set1_arr[idx].push('(you) can\'t listen to the music');
+		word_set2_arr[idx].push('ìŒì•…ì„ ë“¤ìœ¼ë©´ ì•ˆ ë¼ìš”');
+		
+		word_set1_arr[idx].push('but it\'s small');
+		word_set2_arr[idx].push('ìž‘ì€ë°ìš”');
+		
+		word_set1_arr[idx].push('but (i\'m) little bit busy');
+		word_set2_arr[idx].push('ì¢€ ë°”ìœë°ìš”');
+		
+		word_set1_arr[idx].push('but now is raining');
+		word_set2_arr[idx].push('ì§€ê¸ˆ ë¹„ê°€ ì˜¤ëŠ”ëŒ€ìš”');
+		
+		word_set1_arr[idx].push('but (i) have another appointment');
+		word_set2_arr[idx].push('ë‹¤ë¥¸ ì•½ì†ì´ ìžˆëŠ”ë°ìš”');
+		//END HERE
+		
+		word_set1_arr[idx].push('but it is meeting time');
+		word_set2_arr[idx].push('íšŒì˜ ì‹œê°„ì¸ë°ìš”');
+		
+		word_set1_arr[idx].push('but (i) was busy');
+		word_set2_arr[idx].push('ë°”ë¹´ëŠ”ë°ìš”');
+		
+		word_set1_arr[idx].push('but it is my friend');
+		word_set2_arr[idx].push('ì œ ì¹œêµ°ë°ìš”');
+		
+	}
+	
+if (true) {
+		idx++;
+		explain_set[idx] = "ë¬¸ë²• - part D";
+		
+		//01
+		word_set1_arr[idx].push('Can I take your bag?');
+		word_set2_arr[idx].push('ê°€ë°©ì„ ë“¤ì–´ ë“œë¦´ê¹Œìš”?');
+		
+		word_set1_arr[idx].push('How can i help you?');
+		word_set2_arr[idx].push('ë­ ë„ì™€ ë“œë¦´ê¹Œìš”?');
+		
+		word_set1_arr[idx].push('Can I open the door for you?');
+		word_set2_arr[idx].push('ë¬¸ ì—´ì–´ ë“œë¦´ê¹Œìš”');
+		
+		word_set1_arr[idx].push('Could you close the door for me?');
+		word_set2_arr[idx].push('ë¬¸ ì¢€ ë‹«ì•„ ì£¼ì‹œê² ì–´ìš”?');
+		
+		word_set1_arr[idx].push('Could you teach me German?');
+		word_set2_arr[idx].push('ë…ì¼ì–´ ì¢€ ê°€ë¥´ì³ ì£¼ì‹œê² ì–´ìš”?');
+		
+		//06
+		word_set1_arr[idx].push('Could I borrow your pen?');
+		word_set2_arr[idx].push('íŽœ ì¢€ ë¹Œë ¤ ì£¼ì‹œê² ì–´ìš”?');
+		
+		word_set1_arr[idx].push('Minhye said that her feeling is good');
+		word_set2_arr[idx].push('ë¯¼í˜œ ì”¨ê°€ ê¸°ë¶„ì´ ì¢‹ë‹¤ê³  í–ˆì–´ìš”');
+		
+		word_set1_arr[idx].push('Jeny say that lately she is not busy');
+		word_set2_arr[idx].push('ì œë‹ˆ ì”¨ê°€ ìš”ì¦˜ ë°”ì˜ì§€ ì•Šë‹¤ê³  í–ˆì–´ìš”');
+		
+		word_set1_arr[idx].push('Kim Yura said she is healthy');
+		word_set2_arr[idx].push('ê¹€ì¹˜ê°€ ê±´ê°•ì— ì¢‹ë‹¤ê³  í•´ìš”');
+		
+		word_set1_arr[idx].push('Andy said he read the book ');
+		word_set2_arr[idx].push('ì•¤ë”” ì”¨ê°€ ì±…ì„ ì½ëŠ”ë‹¤ê³  í–ˆì–´ìš”');
+		
+		//11
+		word_set1_arr[idx].push('Sora said that right now is raining');
+		word_set2_arr[idx].push('ì†Œë¼ ì”¨ê°€ ì§€ê¸ˆ ë¹„ê°€ ì˜¨ë‹¤ê³  í–ˆì–´ìš”');
+		
+		word_set1_arr[idx].push('Irina said that lately she has not been eating well.');
+		word_set2_arr[idx].push('ì´ë¦¬ë‚˜ ì”¨ê°€ ìš”ì¦˜ ë°¥ì„ ìž˜ ë¨¹ì§€ ì•ŠëŠ”ë‹¤ê³  í–ˆì–´ìš”');
+		
+		//END HERE
+		
+	}
+	
+	
+	/*
+	
+if (false) {
+		idx++;
+		explain_set[idx] = "CHANGE";
+		
+		//01
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//06
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//11
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		//16
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		
+		word_set1_arr[idx].push('eng');
+		word_set2_arr[idx].push('kor');
+		//END HERE
+		
+	}
+	
+	*/
+	
